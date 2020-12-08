b1c86f3d7172e1c55c7bd4b9875b5878ddd4c284
Abir <kh27.abir@gmail.com>
2020-12-08 18:46:45 +0600

    guest and user_cart merged

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -3,9 +3,7 @@ class OrderedItemsController < ApplicationController
   def create
     # ordered_item = ordered_item_params
     # session[:ordered_items] << ordered_item
-    @order = current_order
-    @order.save
-    @ordered_item = @order.ordered_items.create(ordered_item_params)
+    @ordered_item = current_order.ordered_items.create(ordered_item_params)
     @ordered_item.subtotal = ordered_item_params[:price].to_i * ordered_item_params[:quantity].to_i
     @ordered_item.purchase_price = ordered_item_params[:purchase_price].to_i * ordered_item_params[:quantity].to_i
 
@@ -20,16 +18,14 @@ class OrderedItemsController < ApplicationController
   end
 
   def update
-    @order = current_order
-    @ordered_item = @order.ordered_items.find(params[:id])
+    @ordered_item = current_order.ordered_items.find(params[:id])
     @ordered_item.subtotal = ordered_item_params[:price].to_i * ordered_item_params[:quantity].to_i
     @ordered_item.update(ordered_item_params)
     @ordered_items = current_order.ordered_items
   end
 
   def destroy
-    @order = current_order
-    @ordered_item = @order.ordered_items.find(params[:id])
+    @ordered_item = current_order.ordered_items.find(params[:id])
     @ordered_item.destroy
     @ordered_items = current_order.ordered_items
   end
@@ -41,7 +37,4 @@ class OrderedItemsController < ApplicationController
     params.require(:ordered_item).permit( :quantity, :product_variant_id, :price, :total, :purchase_price )
   end
 
-  def set_order
-    @order = current_order
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,16 +6,14 @@ module ApplicationHelper
   end
 
   def current_order
+
     if user_signed_in?
-
-      if current_user.orders.present?
-        order = current_user.orders.last.pending ? current_user.orders.last : current_user.orders.new
+      if current_user.orders.exists?
+        order = current_user.orders.last.pending ? current_user.orders.last : current_user.orders.create
       else
-        order = current_user.orders.new
+        order = current_user.orders.create
       end
-
     else
-
       if session[:guest_cart]
         order = Order.find(session[:guest_cart])
       else
@@ -23,9 +21,20 @@ module ApplicationHelper
         session[:guest_cart] = order.id
       end
       order
-
     end
 
+  end
+
+  def transfer_guest_cart
+    if session[:guest_cart]
+        guest_order = Order.find(session[:guest_cart])
+        guest_order.ordered_items.each do |item|
+          item.order_id = current_order.id
+          item.save
+        end
+        guest_order.destroy
+        session[:guest_cart] = nil
+    end
   end
 
 end
