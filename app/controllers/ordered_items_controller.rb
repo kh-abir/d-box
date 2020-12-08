fbe3720c7e7487c53e2b7197ae028dfdeee67be6
Abir <kh27.abir@gmail.com>
2020-12-08 19:50:41 +0600

    Changes: -path fixed for cart

--- app/controllers/ordered_items_controller.rb
+++ app/controllers/ordered_items_controller.rb
@@ -8,8 +8,7 @@ class OrderedItemsController < ApplicationController
     @ordered_item.purchase_price = ordered_item_params[:purchase_price].to_i * ordered_item_params[:quantity].to_i
 
     if @ordered_item.save
-      flash[:added_to_cart] = "Product has been added to cart"
-      redirect_back(fallback_location: 'back')
+      redirect_to products_path, notice: "Item added to cart"
     else
       flash[:added_to_cart] = "Could not add to cart. Please try again!"
       redirect_back(fallback_location: 'back')
@@ -22,12 +21,15 @@ class OrderedItemsController < ApplicationController
     @ordered_item.subtotal = ordered_item_params[:price].to_i * ordered_item_params[:quantity].to_i
     @ordered_item.update(ordered_item_params)
     @ordered_items = current_order.ordered_items
+    redirect_to cart_path, notice: "Cart updated"
+
   end
 
   def destroy
     @ordered_item = current_order.ordered_items.find(params[:id])
     @ordered_item.destroy
     @ordered_items = current_order.ordered_items
+    redirect_to cart_path, notice: "Item removed from the cart"
   end
 
 
