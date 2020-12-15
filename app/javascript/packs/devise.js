d97c17e2f9226420d97851620b97c8d0f462e42e
Rifat Hasan <rifathasan807@gmail.com>
2020-12-15 18:08:37 +0600

    Changes     -form design

---
+++ app/javascript/packs/devise.js
@@ -0,0 +1,8 @@
+$(function(){
+    var modal = document.getElementById('id01');
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
