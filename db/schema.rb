d8d0a697d1de077410e6645186e2e97db87d2285
Abir <kh27.abir@gmail.com>
2021-01-07 21:33:13 +0600

    SignUp confirmation email activated with letter_opener gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_05_114008) do
+ActiveRecord::Schema.define(version: 2021_01_07_104610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -141,6 +141,9 @@ ActiveRecord::Schema.define(version: 2021_01_05_114008) do
     t.string "contact"
     t.integer "role", default: 0
     t.string "address"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
