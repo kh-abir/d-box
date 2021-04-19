2a4c99e781ee1bdf94d70d4137a6998c02c2c0e5
Abir <kh27.abir@gmail.com>
2021-04-19 20:01:22 +0600

    Letter opener configured for heroku/production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'https://rightcodes-d-box.herokuapp.com/'}
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :letter_opener_web
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new(STDOUT)
