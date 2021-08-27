2ed0f78ecc7bb76273c49a9a9bb30bef671a02b2
Kamrul Hassan <kh27.abir@gmail.com>
2021-08-27 16:41:57 +0600

    assets compilation sets to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,7 +8,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = true
 
-  config.assets.compile = true
+  config.assets.compile = false
   config.active_storage.service = :local
   config.log_level = :debug
 
