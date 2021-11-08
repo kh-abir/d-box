141192a9508c448690a24d7a34cc7160a350cb47
Kamrul Hassan <kh27.abir@gmail.com>
2021-11-09 01:13:01 +0600

    modify product_back_in_stock mailer template

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'test@d-box.com'
   layout 'mailer'
 end
--- app/mailers/product_mailer.rb
+++ app/mailers/product_mailer.rb
@@ -3,10 +3,11 @@ class ProductMailer < ApplicationMailer
   helper :application
   default from: 'test@d-box.com'
 
-  def send_notification
-    @user = User.find(params[:user])
-    @url  = params[:url]
-    @product = ProductVariant.find(params[:product])
+  def send_product_back_in_stock_notification(user_id, product_variant_id)
+    @user = User.find(user_id)
+    @product_variant = ProductVariant.find(product_variant_id)
+    @product = @product_variant.product
+    @url  = product_product_variant_url(@product, @product_variant)
     mail(to: @user.email, subject: 'Product back in stock')
   end
 end
