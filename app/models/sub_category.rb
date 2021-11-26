100fbcafc6c4c13b3b0f70a695f697ea5c7b56b4
Akash Das <aku.das02@gmail.com>
2021-11-26 18:39:58 +0600

    make products_sub_categories dependent to products or subcategories

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,6 +1,6 @@
 class SubCategory < ApplicationRecord
   belongs_to :category
-  has_many :products_sub_categories
+  has_many :products_sub_categories, dependent: :destroy
   has_many :products, through: :products_sub_categories
   has_many :banners
 
